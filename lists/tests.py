57fd692ee10ba1bf8a05e4591bcf09e663365f89
shifeng <song0138@algonquinlive.com>
2024-05-01 18:35:51 -0400

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -14,3 +14,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html_2(self):
         response = self.client.get("/")
         self.assertContains(response, "<title>To-Do lists</title>")
+
+  
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
