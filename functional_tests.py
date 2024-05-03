46b4b95539f7590c3a6e5839781b5eb3b5b812fb
shifeng <song0138@algonquinlive.com>
2024-05-03 13:43:28 -0400

    add table

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):  
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_todo_list(self):  
         # Edith has heard about a cool new online to-do app.
@@ -33,14 +39,17 @@ class NewVisitorTest(unittest.TestCase):
         # "1. Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        table = self.browser.find_element(By.ID,"id_list_table")
-        rows = table.find_elements(By.TAG_NAME,"tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn(
-        "2: Use peacock feathers to make a fly",
-        [row.text for row in rows],
-    )
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+            
+       
+    
         # Satisfied, she goes back to sleep
         self.fail("Finish the test!")
 
