3e84e31b454c6e193944192b3e7f1a4b0bf4907a
shifeng <song0138@algonquinlive.com>
2024-05-03 13:34:20 -0400

    add table

--- functional_tests.py
+++ functional_tests.py
@@ -26,7 +26,8 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute("placeholder"),"Enter a to-do item")
 
         # types Buy peacock teathers into a text box
-        inputbox.send_keys("Buy peacock feathers")
+        # inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("Use peacock feathers to make a fly")
 
         # when hit enter, the page updates, and now the page lists
         # "1. Buy peacock feathers" as an item in a to-do list
@@ -35,7 +36,11 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID,"id_list_table")
         rows = table.find_elements(By.TAG_NAME,"tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers." for row in rows))
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn(
+        "2: Use peacock feathers to make a fly",
+        [row.text for row in rows],
+    )
         # Satisfied, she goes back to sleep
         self.fail("Finish the test!")
 
